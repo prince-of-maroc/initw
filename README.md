d116b1d1f7fce042109ed61212976ba71ba4f785
Rodrick <jesprikovic@gmail.com>
2023-01-30 11:19:41 -0500

    Version 3.0 - Added support for Vite and revamped bash script.

--- README.md
+++ README.md
@@ -1,13 +1,18 @@
 # initw - Single Line Tailwind Initiator
 
-Add Tailwind to your Create React App project with one line.
+Add Tailwind to your project with one line.
 
 ```bash
-  npx initw
+  npx initw [builder]
 ```
+
+Replace `[builder]` with either 'vite' or 'cra', depending on which build tool you are using.
 
 That's it. Everything Tailwind has to offer is now available to you, no repetitive manual set-up required.
 
 ## Features
 
-**initw** only definitively works with Create React App projects. In the future, I plan to expand init-tw to be useable in any project.
+**initw** works with the following JS Build Tools:
+<br>- Vite
+<br>- Create React App
+<br></br>In the future, **intw** will be expanded to include more build tools.
--- bin.js
+++ bin.js
@@ -1,16 +1,59 @@
 #!/usr/bin/env node
 
-var { exec } = require("child_process");
+const { exec } = require("child_process");
+const builder = process.argv[2];
 
-const twConfig =
-  "/** @type {import('tailwindcss').Config} */\nmodule.exports = {\n\tcontent: [\n\t\t'./src/**/*.{js,jsx,ts,tsx}',\n\t],\n\ttheme: {\n\t\textend: {},\n\t},\n\tplugins: [],\n}";
+//
+//
+//
 
-const twInput =
+/* 
+      Define File Templates
+                                */
+// Universal
+const cssIndex =
   "@tailwind base;\n@tailwind components;\n@tailwind utilities;\n";
 
-exec(
-  `npm i -D tailwindcss && echo "${twConfig}" > tailwind.config.js && echo "${twInput}" | cat - ./src/index.css > temp.css && mv temp.css ./src/index.css`,
-  () => {
-    console.log("\n\nTailwindCSS is now ready to use!\n\n");
+// For Create React App
+const craConfig =
+  "/** @type {import('tailwindcss').Config} */\nmodule.exports = {\n\tcontent: [\n\t\t'./src/**/*.{js,jsx,ts,tsx}',\n\t],\n\ttheme: {\n\t\textend: {},\n\t},\n\tplugins: [],\n}";
+
+// For Vite
+const viteConfig =
+  "/** @type {import('tailwindcss').Config} */\nmodule.exports = {\n\tcontent: [\n\t\t'./src/**/*.{js,jsx,ts,tsx}',\n\t\t'./index.html',\n\t],\n\ttheme: {\n\t\textend: {},\n\t},\n\tplugins: [],\n}";
+const postConfig =
+  "module.exports = {\n\tplugins: {\n\t\ttailwindcss: {},\n\n\t\tautoprefixer: {},\n\t},};";
+/*
+        End of File Template Section
+                                            */
+
+//
+//
+//
+
+/* Bash Script */
+function getBashScript(projectBuilder) {
+  let bashScript = null;
+
+  if (projectBuilder == "cra") {
+    // Create React App
+    bashScript = `npm i -D tailwindcss && echo "${craConfig}" > tailwind.config.js && echo "${cssIndex}" | cat - ./src/index.css > temp.css && mv temp.css ./src/index.css`;
+  } else if (builder == "vite") {
+    // Vite
+    bashScript = `npm i -D tailwindcss postcss autoprefixer && echo "${viteConfig}" > tailwind.config.cjs && echo "${postConfig}" > postcss.config.cjs && echo "${cssIndex}" | cat - ./src/index.css > temp.css && mv temp.css ./src/index.css`;
   }
-);
+  return bashScript;
+}
+
+const bashScript = getBashScript(builder);
+if (bashScript) {
+  exec(bashScript, () => {
+    console.log("\n\nTailwindCSS is now ready to use!\n\n");
+  });
+} else {
+  exec("echo", () => {
+    console.log(
+      "\nYou did not add a build-tool argument. Vite and Create-React-App are the supported build-tools. \n\nPlease run either of the following:\nnpx initw vite\nnpx initw cra\n"
+    );
+  });
+}
